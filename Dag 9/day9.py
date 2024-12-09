f2804e5acd0ac096f913adbc31736b570e7e5220
ReinoutVanheule <42320216+ReinoutVanheule@users.noreply.github.com>
2024-12-09 11:50:10 +0100

    Update day9.py

--- Dag 9/day9.py
+++ Dag 9/day9.py
@@ -10,15 +10,13 @@ block = True
 total_block = 0
 for c in lines:
     if block:
-        block  = False
         result += [idblock]*int(c)
         total_block += int(c)
         idblock += 1
     else:
-        block = True
         result += [-1]*int(c)
+    block = not block
 last_id = result[-1]
-
 
 insert_indexes = [i for i,c in enumerate(result) if c == -1]
 for i in range(len(result)-1,0,-1):
@@ -33,28 +31,34 @@ for i in range(len(result)-1,0,-1):
     else:
         break
 
-print(sum(i*j for i,j in zip(range(total_block),result)))
-
-# part 2
+# part 1
+blocks = [(int(length),idblock) for length,idblock in zip(lines[::2],range(10000))]
+free = [int(length) for length in lines[1::2]]
+result = []
 block = True
-idblocklow = 0
-idblockhigh = 19998
-result =[]
-while True:
-    l = int(lines[idblocklow])
-    h = int(lines[idblockhigh])
+while blocks:
+    first = blocks[0]
+    last = blocks[-1]
+    available = free[0]
+    
     if block:
-         result += [idblocklow]*l
-         block = False
-         idblocklow += 2
+        result += [first[1]]*first[0]
+        blocks = blocks[1:]
+        block = False
     else:
-        if l < h:
-            result += []
-        
-        
-        
-        
-        
-        
-        
-        
+        if last[0] == available:
+            result += [last[1]]*last[0]
+            free = free[1:]
+            blocks = blocks[:-1]
+            block = True
+    
+        elif last[0] < available:
+            result += [last[1]]*last[0]
+            blocks = blocks[:-1]
+            free[0] = available-last[0]
+            
+        else:
+            result += [last[1]]*available
+            free = free[1:]
+            blocks[-1] = (last[0]-available,last[1])
+            block = True
