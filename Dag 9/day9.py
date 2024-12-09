cb3261dc5443a821749b3fc278a62a7aaa1214d1
ReinoutVanheule <42320216+ReinoutVanheule@users.noreply.github.com>
2024-12-09 19:38:37 +0100

    Add files via upload

--- Dag 9/day9.py
+++ Dag 9/day9.py
@@ -7,58 +7,41 @@ lines = read_input("inputdag9")[0]
 idblock= 0
 result =[]
 block = True
-total_block = 0
-for c in lines:
-    if block:
-        result += [idblock]*int(c)
-        total_block += int(c)
-        idblock += 1
-    else:
-        result += [-1]*int(c)
-    block = not block
-last_id = result[-1]
+for b,f in zip(lines[::2],lines[1::2]+"0"):
+    result += [idblock]*int(b)+[-1]*int(f)
+    idblock += 1
 
 insert_indexes = [i for i,c in enumerate(result) if c == -1]
-for i in range(len(result)-1,0,-1):
+i = len(result)-1
+while i >= 0 and insert_indexes:
     c = result[i]
     if insert_indexes:
         if c == -1:
             insert_indexes.remove(i)
         else:
-            hole = insert_indexes[0]
-            result[hole] = c
+            result[insert_indexes[0]] = c
             insert_indexes = insert_indexes[1:]
-    else:
-        break
+    i-=1
 
-# part 1
+length_result = sum(map(int,lines[::2]))
+print(sum(a*b for a,b in zip(range(length_result),result)))
+    
+# part 2
 blocks = [(int(length),idblock) for length,idblock in zip(lines[::2],range(10000))]
 free = [int(length) for length in lines[1::2]]
-result = []
-block = True
-while blocks:
-    first = blocks[0]
-    last = blocks[-1]
-    available = free[0]
-    
-    if block:
-        result += [first[1]]*first[0]
-        blocks = blocks[1:]
-        block = False
-    else:
-        if last[0] == available:
-            result += [last[1]]*last[0]
-            free = free[1:]
-            blocks = blocks[:-1]
-            block = True
-    
-        elif last[0] < available:
-            result += [last[1]]*last[0]
-            blocks = blocks[:-1]
-            free[0] = available-last[0]
-            
-        else:
-            result += [last[1]]*available
-            free = free[1:]
-            blocks[-1] = (last[0]-available,last[1])
-            block = True
+result = 20000*8*[0]
+old_free = free.copy()
+
+while len(blocks)>1:
+    length,idc = blocks.pop()
+    idd = sum(old_free[:idc])+sum(x for x,y in blocks[:idc])
+    for i,lenempty in enumerate(free[:idc]):
+        if length <= lenempty:
+            idd = sum(old_free[:i])+sum(x for x,y in blocks[:i+1])+old_free[i]-free[i]
+            free[i] = lenempty-length
+            break
+    result[idd:idd+length] = [idc]*length
+        
+print(sum(a*b for a,b in zip(range(len(result)),result)))
+
+        
