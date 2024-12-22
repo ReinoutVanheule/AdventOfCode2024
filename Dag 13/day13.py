816f3170aecfb7b353d6d717ff54568d8b15efc7
ReinoutVanheule <42320216+ReinoutVanheule@users.noreply.github.com>
2024-12-22 18:30:18 +0100

    Update day13.py

--- Dag 13/day13.py
+++ Dag 13/day13.py
@@ -1,54 +1,19 @@
 from aoc import read_input
-import math
 import re
 from sympy import symbols, Eq, solve
 
 # input
 lines = [list(map(int,re.findall(r'\d+',line))) for line in read_input("inputdag13")]
 
-# part 1 and 2
-k,p = symbols('k,p') 
-
-def lcm(a, b):
-    return abs(a*b) // math.gcd(a, b)
-
-def one_combo(a,b,c):
-    i = 0
-    while True:
-        q,r = divmod(c-i*a,b)
-        if r==0:
-            return (i,q)
-        i+=1
-        if i == b:
-            raise Exception()
-    
-
+# part 1 and 2    
 def claw(A,B,C):
-    try:
-        a,b = one_combo(A[0],B[0],C[0])
-        prod = lcm(A[0],B[0])
-        m = prod//A[0]
-        l = prod//B[0]
-        
-        c,d = one_combo(A[1],B[1],C[1])
-        prod = lcm(A[1],B[1])
-        q = prod//A[1]
-        r = prod//B[1]
-        
-        eq1 = Eq(a+k*m, c+p*q) 
-        eq2 = Eq(b-k*l,d-p*r)
-        
-        solve_eq = solve((eq1, eq2), (k,p))
-        k_value = solve_eq[k]
-        p_value = solve_eq[p]
-        
-        if k_value%1==0 and p_value%1==0:
-            A = a+k_value*m
-            B = b-k_value*l
-            return 3*A+B
-        return 0
-    
-    except:
+    x,y = symbols("x,y")
+    eq1 = Eq(x*A[0]+y*B[0],C[0])
+    eq2 = Eq(x*A[1]+y*B[1],C[1])
+    solve_eq = solve((eq1, eq2), (x,y))
+    if solve_eq[x]%1==0 and solve_eq[y]%1 == 0:
+        return 3*solve_eq[x]+solve_eq[y]
+    else:
         return 0
 
 i = 0
@@ -60,4 +25,5 @@ while i < len(lines):
     money1 += claw(A,B,C)
     money2 += claw(A,B,(C[0]+10000000000000,C[1]+10000000000000))
     i += 4
+    
 print(money1,money2)
